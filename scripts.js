c4039b1684a9e17f431d79383b167ed25b7521ad
Daniel-Gallup <drust328@millardps.org>
2016-10-15 03:36:26 -0500

    Create new json file

--- scripts.js
+++ scripts.js
@@ -1,11 +1,21 @@
-$(document).ready(function() {
-  $.getJSON("https://raw.githubusercontent.com/GETHIP/HW2-Template/master/logan.json", function(data){
+$(document).ready(function() 
+{
+  $.getJSON("https://raw.githubusercontent.com/GETHIP/HW2-Template/master/daniel.json", function(data)
+  {
     console.log(data);
+	
     $("#profile-name").text(data.firstName + " " + data.lastName);
     $("#age").text(data.age);
     $("#profile-picture").attr("src", data.picture);
-    $.each(data.nickNames, function(key, value){
-      $("#nicknames").append("<li>" + value.nick + "</li>");
+	
+    $.each(data.nickNames, function(key, value)
+	{
+		$("#nicknames").append("<li>" + value.nick + "</li>");
     });
+	
+	$.each(data.bookMarks, function(k, v)
+	{
+		$("#bookmarks").append("<li><a src=\"" + v.key + "\">" + v.key + "</li>");
+	});
   });
 });
